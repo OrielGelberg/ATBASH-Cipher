cde8fd0858e09a5815ac3178ad66c38d7ce77353
OrielGelberg <g9093587@gmail.com>
2025-05-15 11:22:06 +0300

    15_05

--- Program.cs
+++ Program.cs
@@ -13,19 +13,16 @@ namespace ATBASH_Cipher
             char Decoder(char c)
             {
                 
-                    int temp = c - 'a';          // כמה צעדים מ-a
-                    temp = 'z' - temp;           // הפוך את הספירה
-                    return (char)temp;           // המר בחזרה ל-char
+                    int temp = c - 'a';          
+                    temp = 'z' - temp;           
+                    return (char)temp;          
                 
 
             }
             string Decryption_of_a_string_cipher(string message)
              {
                 message = message.ToLower();
-                //string newGoodMessage = "";
-                // char currentChar;
-
-                //StringBuilder decryptedMessage = new StringBuilder();
+               
                 string result = "";
 
                 for (int i = 0; i < message.Length; i++)
@@ -33,9 +30,9 @@ namespace ATBASH_Cipher
                     char currentChar = message[i];
 
                     if (currentChar >= 'a' && currentChar <= 'z')
-                        result += Decoder(currentChar); // תו מוצפן
+                        result += Decoder(currentChar); 
                     else
-                        result += currentChar;          // תווים שלא מצפינים (למשל רווחים)
+                        result += currentChar;          
                 }
 
                 return result;
@@ -43,7 +40,7 @@ namespace ATBASH_Cipher
             }
             string NewMessage = "Lfi ulixvh ziv kivkzirmt uli z nzqli zggzxp lm gsv Arlmrhg vmvnb.\r\nGsv ilxpvg fmrgh ziv ivzwb zmw dzrgrmt uli gsv hrtmzo.\r\nYlnyh szev yvvm kozxvw mvzi pvb olxzgrlmh.\r\nMfpsyz urtsgvih ziv hgzmwrmt yb uli tilfmw rmurogizgrlm.\r\nGsv zggzxp droo yv hfwwvm zmw hgilmt -- gsvb dlm’g hvv rg xlnrmt.\r\nDv nfhg hgzb srwwvm zmw pvvk gsv kozm hvxivg fmgro gsv ozhg nlnvmg.\r\nErxglib rh mvzi. Hgzb ivzwb.";
              string decryptedMessage = Decryption_of_a_string_cipher(NewMessage);
-             Console.WriteLine("Decrypted message: " + decryptedMessage);
+             
             Console.ReadLine();
         }
     }
